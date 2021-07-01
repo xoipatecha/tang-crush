ccce3fd69806a5e000a5dfc492f784826e9d029d
xoipatecha <86744356+xoipatecha@users.noreply.github.com>
2021-07-01 19:34:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
     introTitle: 'Hí chào bạn!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích bạn <33',
+    title: 'Trên thế giới này có hơn 7 tỷ nụ cười',
+    desc: 'Nhưng với toi nụ cười của bé mới là đẹp nhất ',
+    btnYes: 'thích bạn luôn <33',
     btnNo: 'Không, bạn bè à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích toi <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Tại sao bạn lại thích toi <3',
+    btnReply: 'Gửi cho Khoa <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'hết dịch anh qua đón đi chơi nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'toi biết mà. Yêu bé nhiều nhiều 😘😘',
+    messDesc: 'Hết dịch toi qua đón công chúa đi chơi nhé.',
+    btnAccept: 'Okii lun <3',
     messLink: 'https://www.facebook.com/Xoipatecha324/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
